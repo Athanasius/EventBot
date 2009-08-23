3780ef54cd9c674bec88916303a8f0062a4e3158
Athanasius <code@miggy.org>
2009-08-24 00:41:22 +0100

    Update DB for voting preference lists
    SQL commands to update the votes TABLE to hold multiple votes per
    election-person tuple.  This is achieved with a new column votes.id,
    with an associated sequence such that we can store a list 'A C D B E'
    and preserve the order when querying the votes per person per election
    for use in a 'borda count' or similar 'specify a preference list' voting
    system.

---
+++ setup/alter-db-for-multiple-pubs-per-person-per-election.sql
@@ -0,0 +1,36 @@
+-- Alter the votes table to include a unique per-vote ID
+-- This is intended to be used as in an ORDER BY clause
+-- of SQL queries on the votes table.
+--
+-- Thus if someone Pe votes on an election E for pubs 'A D B C E'
+-- then the following rows are created:
+--
+-- election person pub id
+-- E        Pe     A   x
+-- E        Pe     D   x+1
+-- E        Pe     B   x+2
+-- E        Pe     C   x+3
+-- E        Pe     E   x+4
+--
+-- and thus Pe's preference order for the pubs is preserved.
+--
+-- As the only intent of this column is use in ORDER BY there
+-- seems no reason to alter the table's PRIMARY KEY definition.
+--
+-- Also note that before this alteration only one vote per person per
+-- pub can be recorded anyway, and indeed that tuple is the PRIMARY KEY
+-- on the table and thus unique, so we don't need to be careful about
+-- assigning different IDs to multiple instances of the same tuple.
+--
+
+-- Create the id, we can't constraint it to NULL yet as the extant rows
+-- have no values for the column.
+ALTER TABLE votes ADD COLUMN id integer;
+-- Create the SEQUENCE and associate it with the COLUMN
+CREATE SEQUENCE votes_id_seq;
+ALTER SEQUENCE votes_id_seq OWNED BY votes.id;
+ALTER TABLE votes ALTER COLUMN id SET DEFAULT nextval('votes_id_seq'::regclass);
+-- Now use the SEQUENCE to assign values to the extant rows
+UPDATE votes SET id = nextval('votes_id_seq');
+-- And finally clean things up by now specifying the new column is NOT NILL
+ALTER TABLE votes ALTER COLUMN id SET NOT NULL;
